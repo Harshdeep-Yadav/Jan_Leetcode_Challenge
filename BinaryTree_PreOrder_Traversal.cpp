1c45de35964bde019769ef589ffabdf595a369f8
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-01-09 15:45:22 +0530

    initial commit

---
+++ BinaryTree_PreOrder_Traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void preorder(TreeNode*node,vector<int>&ans)
+    {
+         if(node==NULL)
+            return;
+        ans.push_back(node->val);
+        preorder(node->left,ans);
+        preorder(node->right,ans);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+      
+        vector<int> ans;
+        preorder(root,ans);
+        return ans;
+    }
+};
